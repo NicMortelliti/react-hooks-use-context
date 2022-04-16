5c11f77c19b9c47096f893589db1e241db37092a
NicMortelliti <npmortelliti@gmail.com>
2022-04-15 20:59:06 -0700

    Add stateful user/setUser to UserProvider

--- src/context/user.js
+++ src/context/user.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Create the context
 const UserContext = React.createContext();
 
 // Create a provider component
 function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
+
   // The value prop of the provider will be our context data
   // This value will be available to child components of this provider
-  const currentUser = {
-    name: "Duane",
-    interests: ["Coding", "Biking", "Words ending in 'ing'"],
-  };
   return (
-    <UserContext.Provider value={currentUser}>{children}</UserContext.Provider>
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
   );
 }
 
