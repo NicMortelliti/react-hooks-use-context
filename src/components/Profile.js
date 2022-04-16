e428eee892832bb058da47788709ca1d8af667f9
NicMortelliti <npmortelliti@gmail.com>
2022-04-15 20:47:29 -0700

    Remove user as a prop. Set user to UserContext instead.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,13 @@ import React, { useContext } from "react";
 import { UserContext } from "../context/user";
 import Interests from "./Interests";
 
-function Profile({ user, theme }) {
+function Profile({ theme }) {
+  // Call useContext with our UserContext
+  const user = useContext(UserContext);
+
+  // Now, we can use the use object just like we would if it was passed as a prop
+  console.log(user);
+
   if (!user) return <h2>Please Login To View Profile</h2>;
   return (
     <div>
