dc5d8b078573a514a768dbfaf3e3af8dec2f0723
NicMortelliti <npmortelliti@gmail.com>
2022-04-15 20:52:18 -0700

    Set user login info to display logged in page

--- src/context/user.js
+++ src/context/user.js
@@ -7,7 +7,13 @@ const UserContext = React.createContext();
 function UserProvider({ children }) {
   // The value prop of the provider will be our context data
   // This value will be available to child components of this provider
-  return <UserContext.Provider value={null}>{children}</UserContext.Provider>;
+  const currentUser = {
+    name: "Duane",
+    interests: ["Coding", "Biking", "Words ending in 'ing'"],
+  };
+  return (
+    <UserContext.Provider value={currentUser}>{children}</UserContext.Provider>
+  );
 }
 
 export { UserContext, UserProvider };
