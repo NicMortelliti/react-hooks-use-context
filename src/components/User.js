bc235719c806d48fd26e120f7efa78a1626998b5
NicMortelliti <npmortelliti@gmail.com>
2022-04-15 20:31:16 -0700

    Add provider component

--- src/components/User.js
+++ src/components/User.js
@@ -2,3 +2,12 @@ import React from "react";
 
 // Create the context
 const UserContext = React.createContext();
+
+// Create a provider component
+function UserProvider({ children }) {
+  // The value prop of the provider will be our context data
+  // This value will be available to child components of this provider
+  return <UserContext.Provider value={null}>{children}</UserContext.Provider>;
+}
+
+export { UserContext, UserProvider };
